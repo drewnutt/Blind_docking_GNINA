86aa5274b606ce00ff4cdf8ffab44d00509e9b0a
drewnutt <anm329@pitt.edu>
2023-07-20 15:52:42 -0400

    now using gnina for docking with or without CNN

--- equi_bind_diffdock_box_gnina.py
+++ equi_bind_diffdock_box_gnina.py
@@ -12,8 +12,8 @@ parser.add_argument('--diffdock_result_path', type=str, default='diffdock/result
 parser.add_argument('--results_path', type=str, default='results', help='Path to save results')
 
 parser.add_argument('--seed', type=str, default='666', help='Random seed for docking')
-parser.add_argument('--date', type=str, default='0217', help='date for result folder naming')
-parser.add_argument('--search_mode', type=str, default='32', help='exhaustiveness for docking searching')
+parser.add_argument('--exhaustiveness', type=str, default='32', help='exhaustiveness for docking searching')
+parser.add_argument('--cnn_scoring', type=str, default='rescore', choices=['rescore','none'], help='whether or not to use cnn for rescoring')
 parser.add_argument('--ori_pocket', action='store_true', default=False, help='use exact pocket of Diffdock output without normalize')
 parser.add_argument('--pocket_size', type=float, default=30, help='normalized pocket size in Angstrom')
 
@@ -22,20 +22,22 @@ args = parser.parse_args()
 input_path = args.input_path
 
 seed = args.seed
-date = args.date
-sf = 'vina'
+if args.cnn_scoring == 'rescore':
+    sf = 'gnina'
+else:
+    sf = 'vina'
 # mode = "detail"
-mode = args.search_mode
+mode = args.exhaustiveness
 normal_pocket = not args.ori_pocket
 pocket_size = args.pocket_size
 if normal_pocket:
-    output_path = os.path.join(args.results_path, mode+'_'+sf+"_diffdock_box_pdbqt_vina_seed"+str(seed) +"_normal"+str(pocket_size))+'_date'+date
+    output_path = os.path.join(args.results_path, mode+'_'+sf+"_diffdock_box_pdbqt_vina_seed"+str(seed) +"_normal"+str(pocket_size))
 else:
-    output_path = os.path.join(args.results_path, mode+'_'+sf+"_diffdock_box_pdbqt_vina_seed"+str(seed))+'_date'+date
+    output_path = os.path.join(args.results_path, mode+'_'+sf+"_diffdock_box_pdbqt_vina_seed"+str(seed))
 
 os.system("mkdir "+output_path)
 
-out_csv = os.path.join(args.results_path, "rmsd_results_{}_{}_diffdock_box_pdbqt_vina_seed{}_date{}.csv".format(mode,sf,seed,date))
+out_csv = os.path.join(args.results_path, "rmsd_results_{}_{}_diffdock_box_pdbqt_vina_seed{}.csv".format(mode,sf,seed))
 with open(out_csv,"w") as f:
     f.write("pdb,rmsd\n")
     
@@ -72,16 +74,16 @@ for config in configs:
             f.write("size_x = {}\nsize_y = {}\nsize_z = {}\n".format(pocket_size,pocket_size,pocket_size))
         else:
             f.write("size_x = {}\nsize_y = {}\nsize_z = {}\n".format(min(size[0],60),min(size[1],60),min(size[2],60)))
-        if mode in ['detail', 'balance']:
-            f.write("search_mode = {}\n".format(mode))
-        else:
-            f.write("exhaustiveness = {}\n".format(mode.split('_')[0]))
+        # if mode in ['detail', 'balance']:
+        #     f.write("search_mode = {}\n".format(mode))
+        # else:
+        f.write("exhaustiveness = {}\n".format(mode.split('_')[0]))
         
         f.write("receptor = {}/{}_protein.pdbqt\nligand = {}\n".format(os.path.join(input_path,pdb),pdb,ligand))
         f.write("out = {}\nseed = {}\nnum_modes = 9\nverbosity = 1\n".format(os.path.join(output_path,pdb+"_ligand_out.pdbqt"), seed))
         
     if not os.path.isfile(os.path.join(output_path,pdb+"_ligand_out.pdbqt")):
-        os.system("vina --config "+new_config)
+        os.system("gnina --config "+new_config)
     if not os.path.isfile(os.path.join(output_path,pdb+"_ligand_out.pdbqt")):
         print("error in "+ pdb)
         with open(out_csv, "a") as out_f:
